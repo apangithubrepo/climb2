ee8664de2246d86597842b854c9a9ddb89a7d300
Arpan <arpansinhaofficial3@gmail.com>
2025-02-02 15:30:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Button from './Components/Button';
+
+function App() {
+  return (
+    <div >
+
+      <form>
+          <Button >Hello</Button>
+      </form>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Button.js
@@ -0,0 +1,14 @@
+
+
+
+import React from 'react'
+
+function Button(props) {
+  return (
+    <div>
+        <button>{props.children}</button>
+    </div>
+  )
+}
+
+export default Button
